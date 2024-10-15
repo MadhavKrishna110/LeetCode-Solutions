10daae4d140a6fa530ebb4f1845ac44b64743388
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2024-10-15 17:53:58 +0530

    C++ Solution
            Submission Statistics:
            Question #: 74
            Language: C++
            Runtime: 6 ms
            Runtime percentile: 21.98
            Memory: 12.356 MB
            Memory percentile: 40.01

--- 74. Search a 2D Matrix/Solution.cpp
+++ 74. Search a 2D Matrix/Solution.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    bool binarySearch(vector<int> arr,int target){
-
-        int n = arr.size();
-        int i=0,j=n-1;
+    bool binarySearch(vector<int> v,int target){
+        int i=0,j=v.size()-1;
         while(i<=j){
             int mid = i+(j-i)/2;
-            if(arr[mid] == target){
+            if(v[mid]==target){
                 return true;
-            } else if(arr[mid]>target){
+            } else if(target<v[mid]){
                 j=mid-1;
             } else {
-                i=mid+1;
+                i = mid+1;
             }
+
         }
         return false;
     }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m= matrix.size();
         int n = matrix[0].size();
-        for(int i=0;i<matrix.size();i++){
+        int i=0;
+        bool ans = false;
+        while(i<m ){
             if(target<=matrix[i][n-1]){
-                return binarySearch(matrix[i],target);
-            }
+                ans = ans | binarySearch(matrix[i],target);
+                if(ans) return true;
+            } 
+            i++;
         }
-        return false;
+        return ans;
     }
 };
