e9995d7166ac491197143516e7facf2ace26b262
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2024-11-25 16:15:20 +0530

    C++ Solution
            Submission Statistics:
            Question #: 70
            Language: C++
            Runtime: 0 ms
            Runtime percentile: 100.00
            Memory: 7.408 MB
            Memory percentile: 66.59

--- 70. Climbing Stairs/Solution.cpp
+++ 70. Climbing Stairs/Solution.cpp
@@ -6,9 +6,14 @@ public:
         return dp[n];
     }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=1;
-        return fun(n,dp);
+        int a=1,b=1;
+        if(n<=1) return 1;
+        for(int i=2;i<=n;i++){
+         int sum = a+b;
+         a=b;
+         b=sum;
+        }
+        return b;
+        
     }
 };
