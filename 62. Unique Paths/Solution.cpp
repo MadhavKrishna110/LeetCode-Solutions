13377ac16eb492c081903be3ba916cb4f36e24fb
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2024-12-02 10:16:20 +0530

    C++ Solution
            Submission Statistics:
            Question #: 62
            Language: C++
            Runtime: 2 ms
            Runtime percentile: 16.47
            Memory: 9.292 MB
            Memory percentile: 5.66

--- 62. Unique Paths/Solution.cpp
+++ 62. Unique Paths/Solution.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
+    int fun(int i,int j,int m, int n,vector<vector<int>> &dp){
+        if(i>=m || j>=n) {
+            return 0;
+        }
+        if(i==m-1 && j==n-1){
+            return 1;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        dp[i][j] = fun(i+1,j,m,n,dp)+fun(i,j+1,m,n,dp);
+        return dp[i][j];
+    }
     int uniquePaths(int m, int n) {
-         int N = n + m - 2;
-            int r = m - 1; 
-            double res = 1;
-            
-            for (int i = 1; i <= r; i++)
-                res = res * (N - r + i) / i;
-            return (int)res;
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return fun(0,0,m,n,dp);
     }
 };
