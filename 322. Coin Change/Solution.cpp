912ec70b7d301edce252805ea44987b1c915863e
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2024-12-05 13:44:06 +0530

    C++ Solution
            Submission Statistics:
            Question #: 322
            Language: C++
            Runtime: 20 ms
            Runtime percentile: 75.63
            Memory: 17.828 MB
            Memory percentile: 51.03

--- 322. Coin Change/Solution.cpp
+++ 322. Coin Change/Solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int fun(int ind,int amount,vector<int>& coins, vector<vector<int>> &dp){
         if(amount==0){
-            return 1;
+            return 0;
         }
         if(dp[ind][amount]!=-1){
             return dp[ind][amount];
@@ -18,8 +18,19 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
 
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int res= fun(0,amount,coins,dp);
-        return res==1e9?-1:res-1;
+        // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        // int res= fun(0,amount,coins,dp);
+        // return res==1e9?-1:res-1;
+
+        vector<int> dp(amount+1,1e9);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.size();j++){
+                if(coins[j]<=i){
+                    dp[i] = min(dp[i],dp[i-coins[j]]+1);
+                }
+            }
+        }
+        return dp[amount]>amount?-1:dp[amount];
     }
 };
