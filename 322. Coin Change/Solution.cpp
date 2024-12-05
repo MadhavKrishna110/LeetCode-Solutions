06310ae6e8623ae5b7d0960f42526f380016749a
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2024-12-05 13:33:28 +0530

    C++ Solution
            Submission Statistics:
            Question #: 322
            Language: C++
            Runtime: 104 ms
            Runtime percentile: 8.19
            Memory: 50.052 MB
            Memory percentile: 17.84

--- 322. Coin Change/Solution.cpp
+++ 322. Coin Change/Solution.cpp
@@ -1,47 +1,25 @@
 class Solution {
 public:
-int minCoins(int ind,int amount,vector<int> coins,vector<vector<int>> &dp){
-    if(amount==0){
-        return 0;
-    }
-    if(ind==0){
-        if(amount%coins[ind]==0){
-            return amount/coins[ind];
+    int fun(int ind,int amount,vector<int>& coins, vector<vector<int>> &dp){
+        if(amount==0){
+            return 1;
         }
-        return 1e9;
-    }
-    if(dp[ind][amount]!=1e9){
-        return dp[ind][amount];
-    }
-    if(coins[ind]<=amount){
-        dp[ind][amount]= min(1+minCoins(ind,amount-coins[ind],coins,dp),minCoins(ind-1,amount,coins,dp));
-    } else {
-        dp[ind][amount] = minCoins(ind-1,amount,coins,dp);
-    }
-    return dp[ind][amount];
-}
-    int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        sort(coins.begin(),coins.end());
-        vector<vector<int>> dp(n+1,vector<int>(amount+1));
-        for(int i=0;i<=amount;i++){
-            dp[0][i]=1e9;
+        if(dp[ind][amount]!=-1){
+            return dp[ind][amount];
         }
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
-        }
-
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=amount;j++){
-                if(coins[i-1]<=j){
-                   // cout<<i<<"-"<<j<<endl;
-                    dp[i][j] = min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
-                } else {
-                    dp[i][j] = dp[i-1][j];
-                }
-                
+        int minm = 1e9;
+        for(int i=ind;i<coins.size();i++){
+            if(coins[i]<=amount){
+                minm = min(minm,1+fun(i,amount-coins[i],coins,dp));
             }
         }
-        return dp[n][amount]==1e9?-1:dp[n][amount];
+        return dp[ind][amount]=minm;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int res= fun(0,amount,coins,dp);
+        return res==1e9?-1:res-1;
     }
 };
