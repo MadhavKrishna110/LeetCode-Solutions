42e81b509d1152fcf5c5e6fc0eaf93f273f39607
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2024-12-07 12:19:16 +0530

    C++ Solution
            Submission Statistics:
            Question #: 1250
            Language: C++
            Runtime: 27 ms
            Runtime percentile: 59.60
            Memory: 27.504 MB
            Memory percentile: 6.68

--- 1250. Longest Common Subsequence/Solution.cpp
+++ 1250. Longest Common Subsequence/Solution.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    int lcs(int ind1,int ind2,string text1,string text2,vector<vector<int>> &dp){
-        if(ind1>=text1.size() || ind2>=text2.size()){
-            return 0;
-        }
+    int fun(int ind1,int ind2,string text1, string text2,vector<vector<int>> &dp){
+        if(ind1<0 || ind2<0) return 0;
 
         if(dp[ind1][ind2]!=-1){
             return dp[ind1][ind2];
         }
-        int pick = 0,notPick=0;
         if(text1[ind1]==text2[ind2]){
-            pick = 1+lcs(ind1+1,ind2+1,text1,text2,dp);
-        } else{
-            notPick = max(lcs(ind1+1,ind2,text1,text2,dp),lcs(ind1,ind2+1,text1,text2,dp));
+            return dp[ind1][ind2] = 1+fun(ind1-1,ind2-1,text1,text2,dp);
         }
 
-        return dp[ind1][ind2] = max(pick,notPick);
+        return dp[ind1][ind2] = max(fun(ind1-1,ind2,text1,text2,dp),fun(ind1,ind2-1,text1,text2,dp));
+
+
+
     }
     int longestCommonSubsequence(string text1, string text2) {
-        int n1=text1.size();
-        int n2=text2.size();
-        vector<vector<int>> dp(n1+1,vector<int> (n2+1,0));
-        for(int i=1;i<=n1;i++){
-            for(int j=1;j<=n2;j++){
+        int m = text1.size();
+        int n = text2.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
                 if(text1[i-1]==text2[j-1]){
                     dp[i][j] = 1+dp[i-1][j-1];
                 } else {
@@ -30,8 +28,6 @@ public:
                 }
             }
         }
-        return dp[n1][n2];
-
-        return lcs(0,0,text1,text2,dp);
+        return dp[m][n];
     }
 };
