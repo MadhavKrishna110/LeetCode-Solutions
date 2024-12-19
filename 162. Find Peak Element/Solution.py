b525b1248dea94a04fec9ba7b6f1e5269c75f6ab
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2024-12-20 01:33:24 +0530

    Python3 Solution
            Submission Statistics:
            Question #: 162
            Language: Python3
            Runtime: 3 ms
            Runtime percentile: 5.58
            Memory: 18.108 MB
            Memory percentile: 7.43

--- 162. Find Peak Element/Solution.py
+++ 162. Find Peak Element/Solution.py
@@ -6,15 +6,15 @@ class Solution:
             return 0
         if nums[len(nums)-1]>nums[len(nums)-2]:
             return len(nums)-1
-        start=1
-        end = len(nums)-2
+        start=0
+        end = len(nums)-1
         while start<end:
             mid = start + (end-start)//2
             if nums[mid]>nums[mid-1] and nums[mid]>nums[mid+1]:
                 return mid
             elif nums[mid]<nums[mid-1]:
                 end=mid-1
-            else:
+            elif nums[mid]<nums[mid+1]:
                 start=mid+1
         
         return start
