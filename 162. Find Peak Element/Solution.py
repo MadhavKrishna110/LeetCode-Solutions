483962a6915439f04e2e7c9503a8061e1a4b66fd
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2024-12-20 01:29:55 +0530

    Python3 Solution
            Submission Statistics:
            Question #: 162
            Language: Python3
            Runtime: 0 ms
            Runtime percentile: 100.00
            Memory: 17.896 MB
            Memory percentile: 7.43

---
+++ 162. Find Peak Element/Solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return 0
+        if nums[0]>nums[1]:
+            return 0
+        if nums[len(nums)-1]>nums[len(nums)-2]:
+            return len(nums)-1
+        start=1
+        end = len(nums)-2
+        while start<end:
+            mid = start + (end-start)//2
+            if nums[mid]>nums[mid-1] and nums[mid]>nums[mid+1]:
+                return mid
+            elif nums[mid]<nums[mid-1]:
+                end=mid-1
+            else:
+                start=mid+1
+        
+        return start
+
+        
