b1073b45a8915ab9881f4e38f06596069d41f76e
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2024-12-25 12:33:44 +0530

    C++ Solution
            Submission Statistics:
            Question #: 208
            Language: C++
            Runtime: 26 ms
            Runtime percentile: 45.50
            Memory: 50.588 MB
            Memory percentile: 35.55

---
+++ 208. Implement Trie (Prefix Tree)/Solution.cpp
@@ -0,0 +1,74 @@
+class Node {
+    Node* links[26];
+    bool isEndOfWord=false;
+public:
+
+    void put(char ch, Node* node){
+        links[ch-'a'] = node;
+    }
+    
+    Node* get(char ch){
+        return links[ch-'a'];
+    }
+
+    bool contains(char ch){
+        return links[ch-'a']!=nullptr;
+    }
+
+    void setEnd(){
+        isEndOfWord= true;
+    }
+
+    bool isEnd(){
+        return isEndOfWord;
+    }
+};
+
+class Trie {
+    Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node* node = root;
+        for(int i=0;i<word.length();i++){
+            if(!node->contains(word[i])){
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+        }
+        node->setEnd();
+    }
+    
+    bool search(string word) {
+        Node* node = root;
+        for(int i=0;i<word.length();i++){
+            if(!node->contains(word[i])){
+                return false;
+            }
+            node = node->get(word[i]);
+        }
+        return node->isEnd();
+    }
+    
+    bool startsWith(string prefix) {
+        Node* node = root;
+        for(int i=0;i<prefix.length();i++){
+            if(!node->contains(prefix[i])){
+                return false;
+            }
+            node = node->get(prefix[i]);
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
