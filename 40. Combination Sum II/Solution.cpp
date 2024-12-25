b2a4ca04b47144ad803d1e75ee336fb396e27ebc
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2024-12-25 21:08:28 +0530

    C++ Solution
            Submission Statistics:
            Question #: 40
            Language: C++
            Runtime: 3 ms
            Runtime percentile: 44.05
            Memory: 17.848 MB
            Memory percentile: 11.53

--- 40. Combination Sum II/Solution.cpp
+++ 40. Combination Sum II/Solution.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    void fun(int ind,vector<int> nums,int target,vector<int> &v,vector<vector<int>> &ans){
+    void fun(int idx,int target,vector<int> &res,vector<int> nums,set<vector<int>> &ans){
         if(target==0){
-            ans.push_back(v);
+            ans.insert(res);
+            return;
+        }
+        if(idx==nums.size() ){
             return;
         }
 
-        for(int i=ind;i<nums.size();i++){
-            if(i!=ind && nums[i]==nums[i-1]) continue;
-            if(nums[i]<=target){
-                v.push_back(nums[i]);
-                fun(i+1,nums,target-nums[i],v,ans);
-                v.pop_back();
-            }
+        for(int i=idx;i<nums.size();i++){
+            if(i>idx && nums[i]==nums[i-1]) continue;
+            if(nums[i]>target) break;
+            res.push_back(nums[i]);
+            fun(i+1,target-nums[i],res,nums,ans);
+            res.pop_back();
         }
+
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
-        vector<vector<int>> ans;
-        vector<int> v;
-        fun(0,candidates,target,v,ans);
-        return ans;
+        set<vector<int>> ans;
+        vector<int> res;
+        fun(0,target,res,candidates,ans);
+        vector<vector<int>> result(ans.begin(),ans.end());
+        return result;
+
     }
 };
