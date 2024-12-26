1026d767feb68559db43e357b7b3059667b91623
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2024-12-26 14:38:14 +0530

    C++ Solution
            Submission Statistics:
            Question #: 131
            Language: C++
            Runtime: 472 ms
            Runtime percentile: 5.01
            Memory: 53.488 MB
            Memory percentile: 78.80

--- 131. Palindrome Partitioning/Solution.cpp
+++ 131. Palindrome Partitioning/Solution.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-    bool isPalindrome(string str,int i,int j){
+    bool isPalin(string str){
+        int i=0,j=str.size()-1;
         while(i<=j){
-            if(str[i]!=str[j]){
-                return false;
-            }
+            if(str[i]!=str[j]) return false;
             i++;
             j--;
         }
         return true;
     }
-    void fun(int ind,string s,vector<string> &v,vector<vector<string>> &ans){
-        if(ind==s.size()){
-            ans.push_back(v);
+    void fun(string str,vector<string> &res,vector<vector<string>> &ans){
+        if(str.size()==0){
+            ans.push_back(res);
             return;
         }
-        for(int i=ind;i<s.size();i++){
-           if(isPalindrome(s,ind,i)){
-                v.push_back(s.substr(ind,i-ind+1));
-                fun(i+1,s,v,ans);
-                v.pop_back();
-           }
+
+        for(int i=1;i<=str.size();i++){
+            string s1 = str.substr(0,i);
+            string s2= str.substr(i);
+            
+            if(isPalin(s1)){
+                cout<<s1<<" "<<s2<<endl;
+                res.push_back(s1);
+                fun(s2,res,ans);
+                res.pop_back();
+            }   
         }
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
-        vector<string> v;
-        fun(0,s,v,ans);
+        vector<string> res;
+        fun(s,res,ans);
         return ans;
     }
 };
