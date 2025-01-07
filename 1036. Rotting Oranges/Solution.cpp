98b93b0ee9fe4c6e3f63d858739cff15ae07fb73
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2025-01-08 01:13:55 +0530

    C++ Solution
          Submission Statistics:
          Question #: 1036
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 16.388 MB
          Memory percentile: 88.65

--- 1036. Rotting Oranges/Solution.cpp
+++ 1036. Rotting Oranges/Solution.cpp
@@ -1,53 +1,61 @@
 class Solution {
 public:
-   bool isValid(int x,int y,int m,int n){
-    if(x<0||x>=m||y<0||y>=n) return false;
-    return true;
-   }
+    bool isValid(int i,int j,int m,int n){
+        if(i<0 || j<0 || i>=m || j>=n){
+            return false;
+        }
+        return true;
+    }
     int orangesRotting(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        queue<pair<int,pair<int,int>>> q;
-        vector<vector<int>> visited(m,vector<int>(n,0));
+        int m = grid.size();
+        int n = grid[0].size();
+        queue<pair<int,int>> q;
         for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[i].size();j++){
-                if( grid[i][j]==2){
-                    q.push({0,{i,j}});
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
                 }
             }
         }
-        int days=0;
+        q.push({-1,-1});
+        int time=0;
         while(!q.empty()){
-            pair<int,int> p = q.front().second;
-            int x=p.first;
-            int y =p.second;
-            int cnt=q.front().first;
+            int x= q.front().first;
+            int y= q.front().second;
             q.pop();
-            days= max(days,cnt);
-
-
-            if(isValid(x+1,y,m,n) && grid[x+1][y]==1){
-                grid[x+1][y]=2;
-                q.push({cnt+1,{x+1,y}});
-            }
-            if(isValid(x-1,y,m,n) && grid[x-1][y]==1){
-                grid[x-1][y]=2;
-                q.push({cnt+1,{x-1,y}});
-            }
-            if(isValid(x,y+1,m,n) && grid[x][y+1]==1){
-                grid[x][y+1]=2;
-                q.push({cnt+1,{x,y+1}});
-            }
-            if(isValid(x,y-1,m,n) && grid[x][y-1]==1){
-                grid[x][y-1]=2;
-                q.push({cnt+1,{x,y-1}});
+            if(x==-1){
+                if(!q.empty()){
+                    time++;
+                    q.push({-1,-1});
+                }
+            } else {
+                if(isValid(x+1,y,m,n) && grid[x+1][y]==1){
+                    grid[x+1][y]=2;
+                    q.push({x+1,y});
+                }
+                if(isValid(x-1,y,m,n) && grid[x-1][y]==1){
+                    grid[x-1][y]=2;
+                    q.push({x-1,y});
+                }
+                if(isValid(x,y+1,m,n) && grid[x][y+1]==1){
+                    grid[x][y+1]=2;
+                    q.push({x,y+1});
+                }
+                if(isValid(x,y-1,m,n) && grid[x][y-1]==1){
+                    grid[x][y-1]=2;
+                    q.push({x,y-1});
+                }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1)return -1;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
             }
         }
-        return days;
+        
+        return time;
+
     }
 };
