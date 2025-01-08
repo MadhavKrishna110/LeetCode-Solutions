73040d07ad196f27d54a4986ca44f273e9f8ca5e
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2025-01-08 19:16:48 +0530

    C++ Solution
          Submission Statistics:
          Question #: 801
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 17.112 MB
          Memory percentile: 59.29

--- 801. Is Graph Bipartite?/Solution.cpp
+++ 801. Is Graph Bipartite?/Solution.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n= graph.size();
+    bool dfs(int node,int color,vector<int>& visited,vector<vector<int>>& graph){
 
-        queue<int> q;
-        vector<int> color(n,-1);
+        visited[node] = color;
+        bool ans=true;
+        for(int i:graph[node]){
+            if(visited[i]==-1){
+                ans = ans && dfs(i,1-color,visited,graph);
+            } else if(visited[i]==visited[node]){
+                return false;
+            }
+        }
+        return ans;
+        
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> visited(n,-1);
 
         for(int i=0;i<n;i++){
-            if(color[i]!=-1) continue;
-
-            q.push(i);
-            color[i]=0;
-            while(!q.empty()){
-                int node = q.front();
-                q.pop();
-            cout<<node<<"-"<<color[node]<<endl;
-                for(int j=0;j<graph[node].size();j++){
-                    int item = graph[node][j];
-                    if(color[item]==-1){
-                        color[item] = 1-color[node];
-                        q.push(graph[node][j]);
-                    } else if(color[item]==color[node]){
-                        return false;
-                    }
-                }
+            if(visited[i]==-1){
+                bool ans = dfs(i,0,visited,graph);
+                if(!ans) return false;
             }
+            
         }
         return true;
     }
