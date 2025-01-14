3971077241389a2e6ffa01200ef06232a705bee9
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2025-01-14 15:49:24 +0530

    C++ Solution
          Submission Statistics:
          Question #: 33
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 15.684 MB
          Memory percentile: 27.33

--- 33. Search in Rotated Sorted Array/Solution.cpp
+++ 33. Search in Rotated Sorted Array/Solution.cpp
@@ -18,16 +18,14 @@ public:
        int partition=-1;
        while(start<end){
         int mid = start+(end-start)/2;
-         if(nums[mid]>nums[mid+1]){
-            partition=mid;
-            break;
-         } else if(nums[mid]>nums[end]){
-            start=mid+1;
+         if(nums[mid]>nums[end]){
+            start= mid+1;
          } else {
             end = mid;
          }
        }
-       cout<<partition<<endl;
+       cout<<start<<"-"<<end<<endl;
+       partition = start-1;
        if(partition==-1){
         return binarySearch(0,nums.size()-1,nums,target);
        } else {
