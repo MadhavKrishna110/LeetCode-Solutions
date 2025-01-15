dc9435e9de9cf68fc90bcd3d4aaf2f338999e175
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2025-01-15 14:27:11 +0530

    C++ Solution
          Submission Statistics:
          Question #: 152
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 17.808 MB
          Memory percentile: 17.53

--- 152. Maximum Product Subarray/Solution.cpp
+++ 152. Maximum Product Subarray/Solution.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n=nums.size();
-        int pre=1;
-        int suff=1;
-        int ans=INT_MIN;
+        int n = nums.size();
+        int prefix=1,suffix=1;
+        int maxm=INT_MIN;
         for(int i=0;i<n;i++){
-            if(pre==0) pre=1;
-            if(suff==0) suff=1;
-            pre = pre*nums[i];
-            suff = suff*nums[n-i-1];
-            ans = max(ans,max(pre,suff));
+            if(prefix==0) prefix=1;
+            if(suffix==0) suffix=1;
+
+            prefix*=nums[i];
+            suffix*=nums[n-i-1];
+            maxm = max(maxm,max(prefix,suffix));
         }
-        return ans;
+        return maxm;
     }
 };
