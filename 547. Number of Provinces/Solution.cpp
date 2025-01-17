e46fea1fe135a8ecf76c559c9e18ba64919ea2cd
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2025-01-17 19:29:11 +0530

    C++ Solution
          Submission Statistics:
          Question #: 547
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 21.124 MB
          Memory percentile: 13.74

--- 547. Number of Provinces/Solution.cpp
+++ 547. Number of Provinces/Solution.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
-    void dfs(int idx,vector<int> adjList,vector<vector<int>>& isConnected,vector<int> &visited){
-        if(idx>=adjList.size()) return;
+    void dfs(int idx,vector<int>& visited,vector<vector<int>> &adjList){
+        if(visited[idx]) return;
 
         visited[idx]=1;
-        for(int i=0;i<adjList.size();i++){
-            if(i!=idx && adjList[i]==1 && !visited[i]){
-                dfs(i,isConnected[i],isConnected,visited);
-            }
+        for(int i:adjList[idx]){
+            dfs(i,visited,adjList);
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
+        vector<vector<int>> adjList(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j && isConnected[i][j]==1){
+                    adjList[i].push_back(j);
+                    adjList[j].push_back(i);
+                }
+            }
+        }
+
         vector<int> visited(n,0);
         int provinces=0;
         for(int i=0;i<n;i++){
             if(!visited[i]){
-                dfs(i,isConnected[i],isConnected,visited);
+                dfs(i,visited,adjList);
                 provinces++;
             }
+
         }
         return provinces;
     }
