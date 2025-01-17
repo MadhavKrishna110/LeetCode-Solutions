02bf0f3bab4ddf94b274871dd5aee5aace622066
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2025-01-17 19:40:25 +0530

    C++ Solution
          Submission Statistics:
          Question #: 733
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 18.428 MB
          Memory percentile: 21.48

--- 733. Flood Fill/Solution.cpp
+++ 733. Flood Fill/Solution.cpp
@@ -1,43 +1,23 @@
 class Solution {
 public:
-    bool isValid(int i,int j, int m,int n){
-        if(i<0 || j<0 || i>=m || j>=n){
-            return false;
+    void dfs(int sr,int sc,int initialColor,int color,vector<vector<int>> &image,vector<vector<int>> &visited){
+        if(sr<0 || sr>=image.size() || sc<0 || sc>=image[0].size()||image[sr][sc]!=initialColor || visited[sr][sc]){
+            return;
         }
-        return true;
+
+        visited[sr][sc]=1;
+        image[sr][sc]=color;
+        dfs(sr-1,sc,initialColor,color,image,visited);
+        dfs(sr+1,sc,initialColor,color,image,visited);
+        dfs(sr,sc-1,initialColor,color,image,visited);
+        dfs(sr,sc+1,initialColor,color,image,visited);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-       int m = image.size();
-       int n = image[0].size();
-
-       queue<pair<int,int>> q;
-       q.push({sr,sc});
+        int m = image.size();
+        int n = image[0].size();
         vector<vector<int>> visited(m,vector<int>(n,0));
-        visited[sr][sc]=1;
-        int tempcol=image[sr][sc];
-       while(!q.empty()){
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
 
-            if(isValid(x+1,y,m,n) && !visited[x+1][y] && image[x+1][y]==tempcol){
-                q.push({x+1,y});
-                visited[x+1][y]=1;
-            }
-            if(isValid(x-1,y,m,n) && !visited[x-1][y] && image[x-1][y]==tempcol){
-                q.push({x-1,y});
-                visited[x-1][y]=1;
-            }
-            if(isValid(x,y+1,m,n) && !visited[x][y+1] && image[x][y+1]==tempcol){
-                q.push({x,y+1});
-                visited[x][y+1]=1;
-            }
-            if(isValid(x,y-1,m,n) && !visited[x][y-1] && image[x][y-1]==tempcol){
-                q.push({x,y-1});
-                visited[x][y-1]=1;
-            }
-            image[x][y] = color;
-       } 
-       return image;
+        dfs(sr,sc,image[sr][sc],color,image,visited);
+        return image;
     }
 };
