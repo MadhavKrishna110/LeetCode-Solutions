a24d120e243aeddd86ef7390c16100478d5a1c65
MadhavKrishna110 <51095713+MadhavKrishna110@users.noreply.github.com>
2025-01-19 14:51:24 +0530

    C++ Solution
          Submission Statistics:
          Question #: 124
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 29.404 MB
          Memory percentile: 6.58

---
+++ 124. Binary Tree Maximum Path Sum/Solution.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int pathSum(TreeNode* root,int &maxSum){
+        if(root==NULL){
+            return -1e9;
+        }
+        if(root->left==NULL && root->right==NULL){
+            return root->val;
+        }
+
+        int left = pathSum(root->left,maxSum);
+        int right = pathSum(root->right,maxSum);
+        maxSum = max(maxSum,max(root->val+left+right,max(left,right)));
+        maxSum = max(max(max(left,right)+root->val,root->val),maxSum);
+        return max(max(left,right)+root->val,root->val);
+    }
+    int maxPathSum(TreeNode* root) {
+        if(root==NULL) return 0;
+        if(root->left==NULL && root->right==NULL) return root->val;
+        int maxm = INT_MIN;
+        int sum = pathSum(root,maxm);
+        return max(maxm,sum);
+        
+    }
+};
